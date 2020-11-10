3af4fac39ade7cac7b1dd99425ba2f6b67bfeaa3
jvanheesch <joris.vanheesch@ae.be>
2020-11-10 17:52:31 +0100

    Added integration test and unit test: maven-surefire-plugin goals are bound by default, while maven-failsafe-plugin goals are not!

---
+++ src/test/java/com/github/jvanheesch/SomeServiceIT.java
@@ -0,0 +1,14 @@
+package com.github.jvanheesch;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SomeServiceIT {
+    @Test
+    public void getSomeString() {
+        SomeService someService = new SomeService();
+        String actual = someService.getSomeString();
+        assertEquals(actual, "ABC");
+    }
+}
---
+++ src/test/java/com/github/jvanheesch/SomeServiceTest.java
@@ -0,0 +1,21 @@
+package com.github.jvanheesch;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SomeServiceTest {
+    @Test
+    public void testTrue() {
+        SomeService someService = new SomeService();
+        boolean actual = someService.returnInput(true);
+        assertTrue(actual);
+    }
+
+    @Test
+    void testFalse() {
+        SomeService someService = new SomeService();
+        boolean actual = someService.returnInput(false);
+        assertFalse(actual);
+    }
+}
